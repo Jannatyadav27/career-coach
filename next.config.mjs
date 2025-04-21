b4b9ce86c2b95d736bf5fd4b049f7c8fe3f4fe4b
jannatyadav27 <jannatyadav270@gmail.com>
2025-04-21 22:19:52 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['randomuser.me'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
